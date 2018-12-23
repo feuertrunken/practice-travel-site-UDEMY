bdee9d84047421135600c0b0fafc8f19e9999536
don <don@livelihood.com>
2018-12-23 15:14:02 -0600

    gulp@4.0.0 giving me probelms; moved down to gulp@3.9.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
 var gulp = require('gulp');
 
 gulp.task('default', function () { console.log("Hey, default"); });
+
+gulp.task('html', function () {
+  console.log("Imagine something useful being done to your HTML herel");
+});
