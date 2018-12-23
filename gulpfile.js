64b60b3f1c1c4f4fdb0242911150de4136c8efa5
don <don@livelihood.com>
2018-12-23 17:06:01 -0600

    End of day; need to debug gruntfile.js tomorrow

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
 
 gulp.task('default', function () { console.log("Hey, default"); });
 
 gulp.task('html', function () {
-  console.log("Imagine something useful being done to your HTML herel");
+  console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('styles', function () {
+  //ssconsole.log("in styles task");
+  return gulp.src('.app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function () {
+  watch('./app/index.html', function () {
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('styles');
+  });
 });
